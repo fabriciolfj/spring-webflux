e43ace7d9d733e26cfd91a9706396ace422c1f89
fabricio <fabricio.jacob@outlook.com>
2020-08-04 21:41:38 -0300

    add

--- src/main/java/com/github/fabriciolfj/springwebflux/SpringWebfluxApplication.java
+++ src/main/java/com/github/fabriciolfj/springwebflux/SpringWebfluxApplication.java
@@ -2,12 +2,29 @@ package com.github.fabriciolfj.springwebflux;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class SpringWebfluxApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringWebfluxApplication.class, args);
+	}
+
+	@GetMapping("/client")
+	public Object getAuthorizedClient(@RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient client) {
+		return client;
+	}
+
+	@GetMapping("/user")
+	public Object getUser(@AuthenticationPrincipal OAuth2User user) {
+		return user;
 	}
 
 }
---
+++ src/main/java/com/github/fabriciolfj/springwebflux/infra/config/SpringWebFluxSecurityConfig.java
@@ -0,0 +1,36 @@
+package com.github.fabriciolfj.springwebflux.infra.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+//@EnableWebFluxSecurity
+public class SpringWebFluxSecurityConfig {
+
+    /*@Bean
+    public SecurityWebFilterChain filter(final ServerHttpSecurity http) {
+        //http.authorizeExchange().anyExchange().authenticated().and().httpBasic().and().formLogin();
+        http.authorizeExchange().pathMatchers("/v1/items/**").permitAll()
+                .pathMatchers(HttpMethod.GET, "/v2/items/**").hasRole("USER")
+                .pathMatchers(HttpMethod.POST, "/v2/items").hasRole("ADMIN")
+                .anyExchange().authenticated().and().httpBasic().and().formLogin();
+
+        http.csrf().disable();
+        return http.build();
+    }
+
+    @Bean
+    public MapReactiveUserDetailsService userDetails() {
+        var user = User.withDefaultPasswordEncoder()
+                .username("fabricio").password("mega").roles("USER").build();
+
+        var admin = User.withDefaultPasswordEncoder()
+                .username("admin").password("admin").roles("ADMIN", "USER").build();
+
+        return new MapReactiveUserDetailsService(user, admin);
+    }*/
+}
